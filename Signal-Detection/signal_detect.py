88443127d066a72f090af1f5b17ec69f9dc3948d
kkf17 <favre.kathleen@orange.fr>
2024-09-12 12:45:14 +0200

    Updt signal_detect.py & EM.py

--- Signal-Detection/signal_detect.py
+++ Signal-Detection/signal_detect.py
@@ -17,25 +17,26 @@ def digitalize(x:List[int], n:int)->List[int]:
 	"""Function that digitalize a signal. """
 	return [i for i in x for _ in range(n)]	
 
-def gaussian_noise(x:List[int], mu:float, sigma:float):
+def gaussian_noise(x:List[int], mu:float, sigma:float)->List[int]:
 	""" Function that adds gaussian noise to binary signal. """
 	return [abs(int(abs(random.gauss(mu, sigma)) < AWGN) - x[i]) for i in range(len(x))]
 
-
-def Neyman_Pearson(x:List[int], gamma:float, sigma:float)->int:
-	""" Function for binary hypothesis testing (with Neyman Pearson coefficient). """
-	sign = [1 for _ in range(len(x))]
-	f = lambda x,y : x*y
-	const = math.log(gamma) * sigma**2 + 1/2 * sum(list(map(f, sign, sign)))
-	T = sum(list(map(f, x, sign)))
-	if T > const:
-		return 1
-	else:
-		return 0	
-				
+					
 
 def signal_detection(x:List[int], gamma:float, sigma:float, n:int)->List[int]:
 	""" Function to detect signal. """
+	
+	def Neyman_Pearson(x:List[int], gamma:float, sigma:float)->int:
+		""" Function for binary hypothesis testing (with Neyman Pearson coefficient). """
+		sign = [1 for _ in range(len(x))]
+		f = lambda x,y : x*y
+		const = math.log(gamma) * sigma**2 + 1/2 * sum(list(map(f, sign, sign)))
+		T = sum(list(map(f, x, sign)))
+		if T > const:
+			return 1
+		else:
+			return 0
+
 	return [Neyman_Pearson(x[i:i+n], gamma, sigma) for i in range(0,len(x),n)]
 
 
